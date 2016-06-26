077d8aaeb3518fe4890925a82163c528ee0daad7
lee <lee@192.168.1.158>
2016-06-25 23:17:44 -0400

    Version with user authentication

---
+++ AppStore_1_4/src/main/java/com/appstore/services/SecurityCoreUserDetailsServiceImpl.java
@@ -0,0 +1,40 @@
+package com.appstore.services;
+
+import java.util.Arrays;
+import java.util.Collection;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.appstore.api.dao.UserDAO;
+import com.appstore.entity.User;
+
+@Service("userDetailsService")
+public class SecurityCoreUserDetailsServiceImpl implements UserDetailsService  {
+	@Autowired
+	private UserDAO userDao;
+	
+	@Transactional(readOnly=true)
+	@Override
+	public UserDetails loadUserByUsername(final String userName) throws UsernameNotFoundException {
+		User user = userDao.getUser(userName);
+		return makeUser(user);
+	}
+	
+    private org.springframework.security.core.userdetails.User makeUser(User user) {
+    	Collection authorities = Arrays.asList(makeGrantedAuthorities(user));
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), true, true, true, true, authorities);
+        
+    }
+
+    private GrantedAuthority[] makeGrantedAuthorities(User user) {
+        GrantedAuthority[] result = new GrantedAuthority[1];
+        result[0] = new SimpleGrantedAuthority("ROLE_USER");
+        return result;
+    }
+}
