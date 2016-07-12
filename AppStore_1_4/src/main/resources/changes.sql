f7c8cfc11f618f2e8ead1ff0b430fd9fd5f9a91d
lee-liao <lee.liao@gmail.com>
2016-07-12 15:30:08 -0400

    Initialize D series id
    INSERT INTO app_info (appid) value('D00000001')

--- AppStore_1_4/src/main/resources/changes.sql
+++ AppStore_1_4/src/main/resources/changes.sql
@@ -1,2 +1,5 @@
+use appstore;
+INSERT INTO app_info (appid) value('D00000001');
+
 ALTER TABLE user MODIFY COLUMN password VARCHAR(60);
 update `user` set `password`='$2a$08$uG8nv6uGRytTHtJwnXm0CeoeXxqzpti2ZC3RycOvRPRq6YGC6hnXe' where `userid`='wuqun1';
