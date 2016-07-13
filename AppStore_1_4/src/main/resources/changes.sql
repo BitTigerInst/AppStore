354f2197ac717338709ecde87edeea88f3325340
lee <lee@192.168.1.6>
2016-07-12 23:19:24 -0400

    Inhouse appid starts from D1

--- AppStore_1_4/src/main/resources/changes.sql
+++ AppStore_1_4/src/main/resources/changes.sql
@@ -1,5 +1,5 @@
 use appstore;
-INSERT INTO app_info (appid) value('D00000001');
+INSERT INTO app_info (appid) value('D1');
 
 ALTER TABLE user MODIFY COLUMN password VARCHAR(60);
 update `user` set `password`='$2a$08$uG8nv6uGRytTHtJwnXm0CeoeXxqzpti2ZC3RycOvRPRq6YGC6hnXe' where `userid`='wuqun1';
