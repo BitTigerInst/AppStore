fcceb1d9a329514ebcb9cd6701054c34400f3875
lee <lee@192.168.1.6>
2016-07-20 02:04:39 -0400

    Untrack

--- AppStore_1_4/target/classes/changes.sql
+++
@@ -1,5 +0,0 @@
-use appstore;
-INSERT INTO app_info (appid) value('D1');
-
-ALTER TABLE user MODIFY COLUMN password VARCHAR(60);
-update `user` set `password`='$2a$08$uG8nv6uGRytTHtJwnXm0CeoeXxqzpti2ZC3RycOvRPRq6YGC6hnXe' where `userid`='wuqun1';
